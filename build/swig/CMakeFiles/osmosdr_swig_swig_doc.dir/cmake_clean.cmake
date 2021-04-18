e942d43f7b4580a630758c8a12149b737720bd0f
random-glitch <random-glitch9@protonmail.com>
2021-04-19 08:08:00 +1000

    added support for plutoSDR

---
+++ build/swig/CMakeFiles/osmosdr_swig_swig_doc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "osmosdr_swig.py"
+  "osmosdr_swigPYTHON_wrap.cxx"
+  "CMakeFiles/osmosdr_swig_swig_doc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/osmosdr_swig_swig_doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/swig/CMakeFiles/pygen_swig_e0819.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "osmosdr_swig.py"
+  "osmosdr_swigPYTHON_wrap.cxx"
+  "CMakeFiles/pygen_swig_e0819"
+  "osmosdr_swig.py"
+  "osmosdr_swig.pyc"
+  "osmosdr_swig.pyo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pygen_swig_e0819.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
